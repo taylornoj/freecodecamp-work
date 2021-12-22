b6e12131494696df76a8fe18b7aa3e17499e4475
Taylor Noj <taylornoj@gmail.com>
2021-12-22 04:46:51 +0000

    add instructions to previous work

--- dnaPair.js
+++ dnaPair.js
@@ -1,3 +1,13 @@
+/* The DNA strand is missing the pairing element. 
+Take each character, get its pair, and return the results as a 2d array */
+
+/* Base pairs are a pair of AT and CG. Match the missing element to the 
+provided character.
+Return the provided character as the first element in each array.
+
+The character and its pair are paired up in an array, and all 
+the arrays are grouped into one encapsulating array.*/
+
 function pairElement(str) {
   dnaPair = [];
   str = str.split("");
--- spinalCase.js
+++ spinalCase.js
@@ -1,3 +1,6 @@
+/* Convert a string to spinal case. 
+Spinal case is all-lowercase-words-joined-by-dashes. */
+
 function spinalCase(str) {
   return str.replace(/([a-z])([A-Z])/g, "$1-$2")
   .toLowerCase()
