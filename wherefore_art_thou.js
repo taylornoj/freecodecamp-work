2dc44293cdedd84a025069183271d821a18b5039
Taylor Noj <taylornoj@gmail.com>
2021-12-22 21:19:23 +0000

    complete pig latin function

--- wherefore_art_thou.js
+++ wherefore_art_thou.js
@@ -8,7 +8,7 @@ in the returned array.
 
 
 function whatIsInAName(collection, source) {
-  let arr = collection.filter(function(item) {
+  let arr = collection.filter(function (item) {
     for (let i in source) {
       if (source[i] !== item[i]) {
         return false;
