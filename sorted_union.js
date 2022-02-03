77e4fb97c96e5e7c4a28a39024a1fbcd5027ee60
Taylor Noj <taylornoj@gmail.com>
2022-02-03 16:11:25 +0000

    code complete for sorted union

--- sorted_union.js
+++ sorted_union.js
@@ -11,3 +11,21 @@ The unique numbers should be sorted by their
 original order, but the final array should not be 
 sorted in numerical order.
 */
+
+function uniteUnique(arr) {
+  let args = [...arguments];
+  let newArr = [];
+  for(let i = 0; i < args.length; i++) {
+    for(let j = 0; j < args[i].length; j++) {
+      if(!newArr.includes(args[i][j])) {
+        newArr.push(args[i][j]);
+      }
+    }
+  }
+
+
+  return newArr;
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
