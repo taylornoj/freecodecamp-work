c9a7eb9917923ffbab89a3a6d0e1b263e5f5e4cb
Taylor Noj <taylornoj@gmail.com>
2022-02-03 16:26:22 +0000

    add starter code for html question

---
+++ convert_HTMLentities.js
@@ -0,0 +1,11 @@
+/*
+Convert the characters &, <, >, " (double quote), 
+and ' (apostrophe), in a string to their corresponding 
+HTML entities.
+*/
+
+function convertHTML(str) {
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
