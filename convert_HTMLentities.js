432413b5f2ef4f1cbaaf05d405ae1fc040e6cc25
Taylor Noj <taylornoj@gmail.com>
2022-02-13 16:46:52 +0000

    complete html entities solution

--- convert_HTMLentities.js
+++ convert_HTMLentities.js
@@ -5,7 +5,29 @@ HTML entities.
 */
 
 function convertHTML(str) {
-  return str;
+  let arr = str.split("");
+
+  for(let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case "&":
+        arr[i] = "&amp;";
+        break;
+      case "<":
+        arr[i] = "&lt;";
+        break;
+      case ">":
+        arr[i] = "&gt;";
+        break;
+      case '"':
+        arr[i] = "&quot;";
+        break;
+      case "'":
+        arr[i] = "&apos;";
+        break;
+    }
+  }
+  arr = arr.join("");
+  return arr;
 }
 
-convertHTML("Dolce & Gabbana");
+console.log(convertHTML("Dolce & Gabbana"));
