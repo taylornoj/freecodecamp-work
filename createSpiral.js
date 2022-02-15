d68b0d54bd4ac77577b347e7170be11985878b3d
Taylor Noj <taylornoj@gmail.com>
2022-02-15 12:27:36 +0000

    initialize createSpiral function, what should be returned when n isn't integer or 0

---
+++ createSpiral.js
@@ -0,0 +1,13 @@
+/*
+Your objective is to complete a function createSpiral(N) 
+that receives an integer N and returns a two-dimensional
+array where numbers from 1 to N2 should be represented 
+as clockwise spiral.
+*/
+
+const createSpiral = n => {
+  if (!Number.isInteger(n) || n < 1) return []
+
+}
+
+console.log(createSpiral(0));
