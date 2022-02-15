921009d1c124370b0f1554888916a34f5961328e
Taylor Noj <taylornoj@gmail.com>
2022-02-15 12:30:40 +0000

    work out to return number of arrays given input

--- createSpiral.js
+++ createSpiral.js
@@ -6,8 +6,16 @@ as clockwise spiral.
 */
 
 const createSpiral = n => {
+  // start with what should be input if !integer or < 1
   if (!Number.isInteger(n) || n < 1) return []
 
+  // work out how to return correct num of arrays given input
+  let returnArray = [];
+  for (let i = 0; i < n; i++) {
+    returnArray[i] = Array();
+  }
+  return returnArray;
 }
 
 console.log(createSpiral(0));
+console.log(createSpiral(2));
