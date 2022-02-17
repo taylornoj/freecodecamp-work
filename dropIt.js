62e9e7c4fb83b11430888ad9de0c462747e86da6
Taylor Noj <taylornoj@gmail.com>
2022-02-17 00:55:57 +0000

    add solution

--- dropIt.js
+++ dropIt.js
@@ -7,3 +7,13 @@ true when the iterated element is passed through it.
 Then return the rest of the array once the condition 
 is satisfied, otherwise, arr should be returned as an empty array.
 */
+
+function dropElements(arr, func, i = 0) {
+  return i < arr.length && !func(arr[i])
+    ? (dropElements(arr.slice(i + 1), func, i))
+    : arr;
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {
+  return n >= 3;
+}));
