9aaa5b35f18f07e45f0e50b56597024c904b9d56
Taylor Noj <taylornoj@gmail.com>
2022-02-17 00:47:54 +0000

    add new drop it chllenge

--- createSpiral.js
+++ createSpiral.js
@@ -12,8 +12,39 @@ const createSpiral = n => {
   // work out how to return correct num of arrays given input
   let returnArray = [];
   for (let i = 0; i < n; i++) {
-    returnArray[i] = Array();
+    returnArray[i] = Array(n);
   }
+
+  // we traverse the array but with each 'dead end' we want to change direction
+
+  /* 
+  set up boundary variables that will 'shrink'
+  everytime an edge has been populated
+  */
+ let topBoundary = 0;
+ let bottomBoundary = n-1;
+ let leftBoundary = 0;
+ let rightBoundary = n-1;
+
+ // counter will be incremented value that populates the array positions
+ let counter = 1;
+
+ // direction variable will be checked to see whether the x or y value should be changed
+ let direction = 'LEFT';
+
+ // these variables are the x and y positions in the matrix and are initialized as the first position
+ let x = 0;
+ let y = 0;
+
+ // start a loop that will run until all boundaries have shrunk past opposite facing boundary
+ while (topBoundary <= bottomBoundary && leftBoundary <= rightBoundary) {
+   
+ }
+
+
+
+
+
   return returnArray;
 }
 
