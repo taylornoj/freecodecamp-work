bd96e5f456f1a0223f52b34bea1e50244efe4ed1
Taylor Noj <taylornoj@gmail.com>
2022-02-20 03:42:38 +0000

    add dropit challenge

--- dropIt.js
+++ dropIt.js
@@ -17,3 +17,11 @@ function dropElements(arr, func, i = 0) {
 console.log(dropElements([1, 2, 3, 4], function(n) {
   return n >= 3;
 }));
+
+console.log(dropElements([0, 1, 0, 1], function(n) {
+  return n === 1;
+}));
+
+console.log(dropElements([1, 2, 3, 4], function(n) {
+  return n > 5;
+}));
