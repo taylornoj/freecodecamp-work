7fc4bc24b5c179f127cdfa61972e2461200bc26c
Taylor Noj <taylornoj@gmail.com>
2022-02-22 03:37:14 +0000

    add solution

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -14,3 +14,23 @@ sumTwoAnd(3) returns 5.
 
 If either argument isn't a valid number, return undefined.
 */
+
+function addTogether() {
+  
+  let [first, second] = arguments;
+
+  if (typeof(first) !== "number") {
+    return undefined;
+  }
+  if (second === undefined) {
+    return (second) => addTogether(first, second);
+  }
+  if (typeof(second) !== "number") {
+    return undefined;
+  }
+  return first + second;
+}
+
+console.log(addTogether(2, 3));
+console.log(addTogether(2, "3"));
+console.log(addTogether(2));
