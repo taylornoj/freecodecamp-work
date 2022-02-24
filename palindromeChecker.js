1125735f0128e0516f16257b2d725fd943273b8d
Taylor Noj <taylornoj@gmail.com>
2022-02-24 03:48:27 +0000

    add directions for palindrome checker

---
+++ palindromeChecker.js
@@ -0,0 +1,9 @@
+/*
+Return true if the given string is a palindrome. 
+Otherwise, return false.
+
+You'll need to remove all non-alphanumeric characters 
+(punctuation, spaces and symbols) and turn everything 
+into the same case (lower or upper case) in order to 
+check for palindromes.
+*/
