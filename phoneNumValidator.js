1d7c5000d5850478f7846a70afd114307379d6ab
Taylor Noj <taylornoj@gmail.com>
2022-02-25 03:33:32 +0000

    add final code

--- phoneNumValidator.js
+++ phoneNumValidator.js
@@ -14,3 +14,11 @@ for US numbers (refer to the tests below for other variants):
 5555555555
 1 555 555 5555
 */
+
+function telephoneCheck(str) {
+  let teleRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return teleRegex.test(str);
+}
+
+console.log(telephoneCheck("555-5555"));
+console.log(telephoneCheck("1 555 555 5555"));
