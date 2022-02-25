2ac2f1bbf79d570e693a98e44a35250862368204
Taylor Noj <taylornoj@gmail.com>
2022-02-25 01:00:12 +0000

    add letter arrays

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -15,3 +15,10 @@ All letters will be uppercase. Do not transform
 any non-alphabetic character (i.e. spaces, punctuation), 
 but do pass them on.
 */
+
+function rot13(str) {
+  let alphabets =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'," ", "-", "_", ".", "&","?", "!", "@", "#", "/"];
+    
+  let alphabets13 = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M', " ", "-", "_", ".", "&","?", "!", "@", "#", "/"];
+    
+}
