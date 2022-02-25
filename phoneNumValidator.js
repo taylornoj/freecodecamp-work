dae5237bbe38e56995ad0d6cb83883b408a11c76
Taylor Noj <taylornoj@gmail.com>
2022-02-25 03:22:49 +0000

    add challenge instructions

---
+++ phoneNumValidator.js
@@ -0,0 +1,16 @@
+/*
+Return true if the passed string looks like a 
+valid US phone number.
+
+The user may fill out the form field any way they 
+choose as long as it has the format of a valid US 
+number. The following are examples of valid formats 
+for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+*/
